5c00c30845dfc344b516eb4c0a69b08c66933f7a
sighakalaurent <nguifosighaka>
2023-10-20 11:40:40 +0200

    forme

---
+++ script.js
@@ -0,0 +1,6 @@
+function recuperation() {
+  const title = document.getElementById("title");
+  const description = document.getElementById("description");
+  const jour = document.getElementById("jour");
+  const valider=document.getElementById("valider")
+}
